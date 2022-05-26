536975f919bbda11b6c42eae9bd929cb72fa0ceb
BuildTools <unconfigured@null.spigotmc.org>
2022-05-25 21:25:03 -0300

    first commit 1.1

--- src/database.js
+++ src/database.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose')
 const { NOTES_APP_MONGODB_HOST, NOTES_APP_MONGODB_DATABASE, MONGODB_ATLAS } = process.env;
 //const MONGODB_URI = `mongodb://${NOTES_APP_MONGODB_HOST}/${NOTES_APP_MONGODB_DATABASE}`
 
-mongoose.connect(MONGODB_ATLAS, {
+mongoose.connect('mongodb+srv://All:123@cluster0.4ube8.mongodb.net/notes-app?retryWrites=true&w=majority', {
     useUnifiedTopology: true,
     useNewUrlParser: true
 })
